e87441b1bc8782a6f4c2eb102e05df2549df48cb
Larry512 <jaredpotter1@gmail.com>
2019-03-17 20:42:28 -0600

    And this.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <div className="product">
+
+        </div>
         <Product
           title={'Bananas'}
           imageUrl={'https://i5.walmartimages.com/asr/ad01e2d0-c67e-463f-9caf-b7c1995f40b3_1.7f58d030e8c04579b77174e274d31747.jpeg?odnWidth=200&odnHeight=200&odnBg=ffffff'}
