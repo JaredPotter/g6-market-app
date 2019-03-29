27448f6be1d9851ea140e920f9975297c7ba458a
Larry512 <jaredpotter1@gmail.com>
2019-03-28 20:20:36 -0600

    Updates

---
+++ precache-manifest.2ea127ae23ecb9ff4c9eb46c7db9d970.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2dab2a60e5e241c7fbee",
+    "url": "/static/js/main.4f5da04b.chunk.js"
+  },
+  {
+    "revision": "54df10a823aebb98176e",
+    "url": "/static/js/2.a6c925a8.chunk.js"
+  },
+  {
+    "revision": "2dab2a60e5e241c7fbee",
+    "url": "/static/css/main.283e3da8.chunk.css"
+  },
+  {
+    "revision": "4bfaffa8c291e69836a739e6c4a0251a",
+    "url": "/index.html"
+  }
+];
